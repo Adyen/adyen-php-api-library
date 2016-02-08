d349e80f2e0db9ab572b5b80f465d44d79b7c017
rikterbeek <rikterbeek@me.com>
2016-02-08 14:35:35 +0100

    Implement disable service with requiredFields and endpoint

--- src/Adyen/Service/Resource/Recurring/Disable.php
+++ src/Adyen/Service/Resource/Recurring/Disable.php
@@ -4,5 +4,15 @@ namespace Adyen\Service\Resource\Recurring;
 
 class Disable extends \Adyen\Service\Resource
 {
+    protected $_requiredFields = array(
+        'merchantAccount',
+        'shopperReference'
+    );
+
+    public function __construct($service)
+    {
+        $endpoint = $service->getClient()->getConfig()->get('endpoint') . '/pal/servlet/Recurring/' . $service->getClient()->getApiVersion() . '/disable';
+        parent::__construct($service, $endpoint, $this->_requiredFields);
+    }
 
 }
