11cb5ec80c32178c0790a3fbbda0394056dc9d3f
Rik ter Beek <rikterbeek@me.com>
2016-09-08 12:01:21 +0200

    fix formatting

--- src/Adyen/Util/Util.php
+++ src/Adyen/Util/Util.php
@@ -2,36 +2,33 @@
 
 namespace Adyen\Util;
 
-
 class Util
 {
 
     public static function calculateSha256Signature($hmacKey, $params)
     {
         // validate if hmacKey is provided
-        if($hmacKey == "") {
+        if ($hmacKey == "") {
             throw new \Adyen\AdyenException("You did not provide a HMAC key");
         }
 
-        if(empty($params)) {
+        if (empty($params)) {
             throw new \Adyen\AdyenException("You did not provide any parameters");
         }
 
         // The character escape function
-        $escapeval = function($val) {
-            return str_replace(':','\\:',str_replace('\\','\\\\',$val));
+        $escapeval = function ($val) {
+            return str_replace(':', '\\:', str_replace('\\', '\\\\', $val));
         };
 
         // Sort the array by key using SORT_STRING order
         ksort($params, SORT_STRING);
 
         // Generate the signing data string
-        $signData = implode(":",array_map($escapeval,array_merge(array_keys($params), array_values($params))));
+        $signData = implode(":", array_map($escapeval, array_merge(array_keys($params), array_values($params))));
 
         // base64-encode the binary result of the HMAC computation
-        $merchantSig = base64_encode(hash_hmac('sha256',$signData,pack("H*" , $hmacKey),true));
+        $merchantSig = base64_encode(hash_hmac('sha256', $signData, pack("H*", $hmacKey), true));
         return $merchantSig;
     }
-
-
 }
