e7b42bfef83a042e72230ee809c9366a2ecf7268
Rik ter Beek <rikterbeek@me.com>
2017-07-03 12:08:47 +0200

    #29 Add status and ErrorType into the AdyenException

--- src/Adyen/AdyenException.php
+++ src/Adyen/AdyenException.php
@@ -7,8 +7,39 @@ use Exception;
 class AdyenException extends Exception
 {
 
-//"status": 403,
-//"errorCode": "901",
-//"message": "Invalid Merchant Account",
-//"errorType": "security"
+    protected $_status;
+    protected $_errorType;
+
+    /**
+     * AdyenException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Exception|null $previous
+     * @param null $status
+     * @param null $errorType
+     */
+    public function __construct($message = "", $code = 0, Exception $previous = null, $status = null, $errorType = null)
+    {
+        $this->_status = $status;
+        $this->_errorType = $errorType;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * Get status
+     *
+     * @return null
+     */
+    public function getStatus()
+    {
+        return $this->_status;
+    }
+
+    /**
+     * Get Adyen Error type
+     */
+    public function getErrorType()
+    {
+        return $this->_errorType;
+    }
 }
