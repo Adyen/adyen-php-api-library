0e6530599af0d45d198e9f8b47a380879bae66da
Giorgos Adam <georgios.adam@adyen.com>
2018-11-27 15:58:09 -0800

    Added authorise3ds2 consumer

--- src/Adyen/Service/Payment.php
+++ src/Adyen/Service/Payment.php
@@ -15,6 +15,11 @@ class Payment extends \Adyen\Service
 	protected $_authorise3D;
 
 	/**
+	 * @var ResourceModel\Payment\Authorise3DS2
+	 */
+	protected $_authorise3DS2;
+
+	/**
 	 * Payment constructor.
 	 *
 	 * @param \Adyen\Client $client
@@ -25,6 +30,7 @@ class Payment extends \Adyen\Service
 		parent::__construct($client);
 		$this->_authorise = new \Adyen\Service\ResourceModel\Payment\Authorise($this);
 		$this->_authorise3D = new \Adyen\Service\ResourceModel\Payment\Authorise3D($this);
+		$this->_authorise3DS2 = new \Adyen\Service\ResourceModel\Payment\Authorise3DS2($this);
 	}
 
 	/**
@@ -46,6 +52,17 @@ class Payment extends \Adyen\Service
 	public function authorise3D($params)
 	{
 		$result = $this->_authorise3D->request($params);
+		return $result;
+	}
+
+	/**
+	 * @param $params
+	 * @return mixed
+	 * @throws \Adyen\AdyenException
+	 */
+	public function authorise3DS2($params)
+	{
+		$result = $this->_authorise3DS2->request($params);
 		return $result;
 	}
 }
---
+++ src/Adyen/Service/ResourceModel/Payment/Authorise3DS2.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Adyen\Service\ResourceModel\Payment;
+
+class Authorise3DS2 extends \Adyen\Service\AbstractResource
+{
+	/**
+	 * @var string
+	 */
+	protected $_endpoint;
+
+	/**
+	 * Include applicationInfo key in the request parameters
+	 *
+	 * @var bool
+	 */
+	protected $allowApplicationInfo = true;
+
+	/**
+	 * Authorise3DS2 constructor.
+	 *
+	 * @param \Adyen\Service $service
+	 */
+    public function __construct($service)
+    {
+        $this->_endpoint = $service->getClient()->getConfig()->get('endpoint') . '/pal/servlet/Payment/' . $service->getClient()->getApiPaymentVersion() . '/authorise3ds2';
+        parent::__construct($service, $this->_endpoint, $this->allowApplicationInfo);
+    }
+}
