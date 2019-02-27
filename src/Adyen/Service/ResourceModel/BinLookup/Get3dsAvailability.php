da5a23e708a4b846df2e41ca86d3fa5eb996f01f
Fabricio Bellesa <fabricio.bellesa@picpay.com>
2019-02-27 10:15:01 -0300

    Adding Get3dsAvailability into BinLookup directory

---
+++ src/Adyen/Service/ResourceModel/BinLookup/Get3dsAvailability.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Adyen\Service\ResourceModel\BinLookup;
+
+class Get3dsAvailability extends \Adyen\Service\AbstractResource
+{
+    /**
+     * @var string
+     */
+    protected $_endpoint;
+
+    /**
+     * Include applicationInfo key in the request parameters
+     *
+     * @var bool
+     */
+    protected $allowApplicationInfo = true;
+
+    /**
+     * Authorise constructor.
+     *
+     * @param \Adyen\Service $service
+     */
+    public function __construct($service)
+    {
+        $this->_endpoint = $service->getClient()->getConfig()->get('endpoint') . '/pal/servlet/BinLookup/get3dsAvailability';
+        parent::__construct($service, $this->_endpoint, $this->allowApplicationInfo);
+    }
+}
+
