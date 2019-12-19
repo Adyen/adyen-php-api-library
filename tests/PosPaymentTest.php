40a5b8b3255874287755dc03a275c662be9cafd8
alessio <alessio.zampatti@adyen.com>
2019-12-19 13:48:50 +0100

    PW-1188: add e2e tests

--- tests/PosPaymentTest.php
+++ tests/PosPaymentTest.php
@@ -9,7 +9,7 @@ class PosPaymentTest extends TestCase
 
     public function testCreatePosPaymentSuccess()
     {
-        if (empty($this->settings['POIID']) || $this->settings['POIID'] = 'UNIQUETERMINALID') {
+        if (empty($this->settings['POIID']) || $this->settings['POIID'] == 'UNIQUETERMINALID') {
             $this->skipTest("Skipped the test. Configure your POIID in the config");
         }
 
@@ -74,7 +74,7 @@ class PosPaymentTest extends TestCase
     public function testCreatePosPaymentDeclined()
     {
 
-        if (empty($this->settings['POIID']) || $this->settings['POIID'] = 'UNIQUETERMINALID') {
+        if (empty($this->settings['POIID']) || $this->settings['POIID'] == 'UNIQUETERMINALID') {
             $this->skipTest("Skipped the test. Configure your POIID in the config");
         }
 
@@ -138,7 +138,7 @@ class PosPaymentTest extends TestCase
 
     public function testCreatePosEMVRefundSuccess()
     {
-        if (empty($this->settings['POIID']) || $this->settings['POIID'] = 'UNIQUETERMINALID') {
+        if (empty($this->settings['POIID']) || $this->settings['POIID'] == 'UNIQUETERMINALID') {
             $this->skipTest("Skipped the test. Configure your POIID in the config");
         }
 
@@ -200,13 +200,190 @@ class PosPaymentTest extends TestCase
 
     }
 
+    public function testGetConnectedTerminals()
+    {
+        // initialize client
+        $client = $this->createTerminalCloudAPIClient();
+
+        // initialize service
+        $service = new Service\PosPayment($client);
+
+        //Construct request
+        $json = '{
+                    "merchantAccount": "' . $this->settings['merchantAccount'] . '"
+                }';
+
+        $params = json_decode($json, true); //Create associative array for passing along
+
+        try {
+            $result = $service->getConnectedTerminals($params);
+        } catch (\Exception $e) {
+            $this->validateApiPermission($e);
+        }
+
+        $this->assertTrue(isset($result['uniqueTerminalIds']));
+    }
+
+    public function testCreatePosPaymentSuccessWithOneclickQuerystring()
+    {
+        if (empty($this->settings['POIID']) || $this->settings['POIID'] == 'UNIQUETERMINALID') {
+            $this->skipTest("Skipped the test. Configure your POIID in the config");
+        }
+
+        // initialize client
+        $client = $this->createTerminalCloudAPIClient();
+        // initialize service
+        $service = new Service\PosPayment($client);
+
+        //Construct request
+        $transactionType = \Adyen\TransactionType::NORMAL;
+        $serviceID = date("dHis");
+        $timeStamper = date("Y-m-d") . "T" . date("H:i:s+00:00");
+
+        $json = '{
+                    "SaleToPOIRequest": {
+                        "MessageHeader": {
+                            "MessageType": "Request",
+                            "MessageClass": "Service",
+                            "MessageCategory": "Payment",
+                            "SaleID": "PosTestLibrary",
+                            "POIID": "' . $this->getPOIID() . '",
+                            "ProtocolVersion": "3.0",
+                            "ServiceID": "' . $serviceID . '"
+                        },
+                        "PaymentRequest": {
+                            "SaleData": {
+                                "SaleTransactionID": {
+                                    "TransactionID": "POSauthWithOneclickQuerystring",
+                                    "TimeStamp": "' . $timeStamper . '"
+                                },
+                                "TokenRequestedType": "Customer",
+                                "SaleReferenceID": "SalesRefABC"
+                            },
+                            "PaymentTransaction": {
+                                "AmountsReq": {
+                                    "Currency": "EUR",
+                                    "RequestedAmount": ' . 14.91 . '
+                                }
+                            },
+                            "PaymentData": {
+                                "PaymentType": "' . $transactionType . '"
+                            }
+                        }
+                    }
+                }
+            ';
+
+        $params = json_decode($json, true); //Create associative array for passing along
+
+        $recurringDetails = array(
+            'shopperEmail' => "save@oneclick.card",
+            'shopperReference' => strval(300),
+            'recurringContract' => "ONECLICK"
+        );
+        $params['SaleToPOIRequest']['PaymentRequest']['SaleData']['SaleToAcquirerData'] = http_build_query($recurringDetails);
+        try {
+            $result = $service->runTenderSync($params);
+            print_r($result);
+        } catch (\Exception $e) {
+            $this->validateApiPermission($e);
+        }
+
+        $this->assertTrue(isset($result['SaleToPOIResponse']));
+        $this->assertEquals('Success', $result['SaleToPOIResponse']['PaymentResponse']['Response']['Result']);
+        try {
+            $additionalResponse = json_decode(base64_decode($result['SaleToPOIResponse']['PaymentResponse']['Response']['AdditionalResponse']), true);
+            $this->assertNotNull($additionalResponse['additionalData']['recurring.recurringDetailReference']);
+        }
+        catch (\Exception $e){
+            $this->fail();
+        }
+
+    }
+
+    public function testCreatePosPaymentSuccessWithOneclickBase64()
+    {
+        if (empty($this->settings['POIID']) || $this->settings['POIID'] == 'UNIQUETERMINALID') {
+            $this->skipTest("Skipped the test. Configure your POIID in the config");
+        }
+
+        // initialize client
+        $client = $this->createTerminalCloudAPIClient();
+        // initialize service
+        $service = new Service\PosPayment($client);
+
+        //Construct request
+        $transactionType = \Adyen\TransactionType::NORMAL;
+        $serviceID = date("dHis");
+        $timeStamper = date("Y-m-d") . "T" . date("H:i:s+00:00");
+
+        $json = '{
+                    "SaleToPOIRequest": {
+                        "MessageHeader": {
+                            "MessageType": "Request",
+                            "MessageClass": "Service",
+                            "MessageCategory": "Payment",
+                            "SaleID": "PosTestLibrary",
+                            "POIID": "' . $this->getPOIID() . '",
+                            "ProtocolVersion": "3.0",
+                            "ServiceID": "' . $serviceID . '"
+                        },
+                        "PaymentRequest": {
+                            "SaleData": {
+                                "SaleTransactionID": {
+                                    "TransactionID": "POSauthWithOneclickBase64",
+                                    "TimeStamp": "' . $timeStamper . '"
+                                },
+                                "TokenRequestedType": "Customer",
+                                "SaleReferenceID": "SalesRefABC"
+                            },
+                            "PaymentTransaction": {
+                                "AmountsReq": {
+                                    "Currency": "EUR",
+                                    "RequestedAmount": ' . 14.91 . '
+                                }
+                            },
+                            "PaymentData": {
+                                "PaymentType": "' . $transactionType . '"
+                            }
+                        }
+                    }
+                }
+            ';
+
+        $params = json_decode($json, true); //Create associative array for passing along
+
+        $recurringDetails = array(
+            'shopperEmail' => "save@oneclick.card",
+            'shopperReference' => strval(301),
+            'recurringContract' => "ONECLICK"
+        );
+        $params['SaleToPOIRequest']['PaymentRequest']['SaleData']['SaleToAcquirerData'] = base64_encode(json_encode($recurringDetails));
+        try {
+            $result = $service->runTenderSync($params);
+            print_r($result);
+        } catch (\Exception $e) {
+            $this->validateApiPermission($e);
+        }
+
+        $this->assertTrue(isset($result['SaleToPOIResponse']));
+        $this->assertEquals('Success', $result['SaleToPOIResponse']['PaymentResponse']['Response']['Result']);
+        try {
+            $additionalResponse = json_decode(base64_decode($result['SaleToPOIResponse']['PaymentResponse']['Response']['AdditionalResponse']),
+                true);
+            $this->assertNotNull($additionalResponse['additionalData']['recurring.recurringDetailReference']);
+        } catch (\Exception $e) {
+            $this->fail();
+        }
+    }
+
     /**
      * After timeout, an Exception will be returned with code: CURLE_OPERATION_TIMEOUTED
      * @covers \Adyen\HttpClient\CurlClient::handleCurlError
      */
     public function testPosPaymentFailTimeout()
     {
-        if (empty($this->settings['POIID']) || $this->settings['POIID'] = 'UNIQUETERMINALID') {
+        if (empty($this->settings['POIID']) || $this->settings['POIID'] == 'UNIQUETERMINALID') {
             $this->skipTest("Skipped the test. Configure your POIID in the config");
         }
 
@@ -268,28 +445,5 @@ class PosPaymentTest extends TestCase
 
     }
 
-    public function testGetConnectedTerminals()
-    {
-        // initialize client
-        $client = $this->createTerminalCloudAPIClient();
-
-        // initialize service
-        $service = new Service\PosPayment($client);
-
-        //Construct request
-        $json = '{
-                    "merchantAccount": "' . $this->settings['merchantAccount'] . '"
-                }';
-
-        $params = json_decode($json, true); //Create associative array for passing along
-
-        try {
-            $result = $service->getConnectedTerminals($params);
-        } catch (\Exception $e) {
-            $this->validateApiPermission($e);
-        }
-
-        $this->assertTrue(isset($result['uniqueTerminalIds']));
-    }
 
 }
