8f52da240d47bfae34e40bfa2b639d82f6466f7b
Ángel Campos <angel.campos@adyen.com>
2020-05-18 13:29:39 +0200

    [PW-2387]: IP validation function  (#193)
    * [PW-2387]: IP address validation util class
    
    * [PW-2387]: Removing reverse-resolve function
    
    * [PW-2387]: Fixes

---
+++ tests/Unit/Util/IpAddressTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen API Library for PHP
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ */
+
+namespace Adyen\Util;
+
+class IpAddressTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testGetAdyenIpAddresses()
+    {
+        $ipAddress = new IpAddress();
+        $this->assertTrue(is_array($ipAddress->getAdyenIpAddresses()));
+    }
+}
