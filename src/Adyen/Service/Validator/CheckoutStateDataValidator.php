4d8d3e678701da03748a96c5da64b920aa1400a1
alexandros <alexandros.moraitis@adyen.com>
2020-06-19 16:48:30 +0200

    Add validator request

---
+++ src/Adyen/Service/Validator/CheckoutStateDataValidator.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Adyen\Service\Validator;
+
+
+class CheckoutStateDataValidator
+{
+    protected $stateDataRootKeys = array(
+        'paymentMethod',
+        'billingAddress',
+        'deliveryAddress',
+        'riskData',
+        'shopperName',
+        'dateOfBirth',
+        'telephoneNumber',
+        'shopperEmail',
+        'countryCode',
+        'socialSecurityNumber',
+        'browserInfo',
+        'installments',
+        'storePaymentMethod',
+        'conversionId'
+    );
+
+    /**
+     * @param array $stateData
+     * @return array
+     */
+    public function getValidatedAdditionalData($stateData)
+    {
+        // Get validated state data array
+        if (!empty($stateData)) {
+            $stateData = DataArrayValidator::getArrayOnlyWithApprovedKeys($stateData, $this->stateDataRootKeys);
+        }
+        return $stateData;
+    }
+}
---
+++ tests/Integration/ValidatorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Adyen\Tests\Integration;
+
+
+use Adyen\TestCase;
+
+class ValidatorTest extends TestCase
+{
+    public function testCheckoutStateDataValidator()
+    {
+        $params = array(
+            'paymentMethod' => 'ideal',
+            'billingAddress' => 'Street 1',
+            'shopperName' => 'Smith',
+            'WrongData' => 'fake data'
+        );
+
+        $checkoutStateDataValidator = new \Adyen\Service\Validator\CheckoutStateDataValidator();
+        $result = $checkoutStateDataValidator->getValidatedAdditionalData($params);
+
+        $this->assertTrue(array_key_exists('paymentMethod', $result));
+        $this->assertTrue(array_key_exists('billingAddress', $result));
+        $this->assertTrue(array_key_exists('shopperName', $result));
+        $this->assertTrue(!array_key_exists('WrongData', $result));
+    }
+}
