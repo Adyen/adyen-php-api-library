4dc2c412934a4e7b910e93da3d340971f49752f0
Timur Abdumuminov <timur.abdumuminov@gmail.com>
2020-10-21 16:18:30 +0200

    Add support for referral API

---
+++ src/Adyen/Service/RiskManagement.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Adyen\Service;
+
+class RiskManagement extends \Adyen\Service
+{
+    /**
+     * @var ResourceModel\RiskManagement\SubmitReferrals
+     */
+    protected $submitReferrals;
+
+    /**
+     * RiskManagement constructor.
+     *
+     * @param \Adyen\Client $client
+     * @throws \Adyen\AdyenException
+     */
+    public function __construct(\Adyen\Client $client)
+    {
+        parent::__construct($client);
+        $this->submitReferrals = new \Adyen\Service\ResourceModel\RiskManagement\SubmitReferrals($this);
+    }
+
+    /**
+     * @param $params
+     * @param null $requestOptions
+     * @return mixed
+     * @throws \Adyen\AdyenException
+     */
+    public function submitReferrals($params, $requestOptions = null)
+    {
+        return $this->submitReferrals->request($params, $requestOptions);
+    }
+}
