c1d22fea80e326347229bb01a08acf74bf4e615a
alexandros <alexandros.moraitis@adyen.com>
2020-12-11 11:18:15 +0100

    extend the list for open invoice and afterpaytouch payment methods

--- src/Adyen/Util/OpenInvoice.php
+++ src/Adyen/Util/OpenInvoice.php
@@ -31,6 +31,13 @@ class OpenInvoice
     const AFTERPAY_DIRECTDEBIT_PAYMENT_METHOD = 'afterpay_directdebit';
     const AFTERPAYTOUCH_PAYMENT_METHOD = 'afterpaytouch';
 
+    const AFTERPAYTOUCH = "afterpaytouch";
+    const AFTERPAYTOUCH_AU = "afterpaytouch_AU";
+    const AFTERPAYTOUCH_CA = "afterpaytouch_CA";
+    const AFTERPAYTOUCH_NZ = "afterpaytouch_NZ";
+    const AFTERPAYTOUCH_US = "afterpaytouch_US";
+    const AFTERPAYTOUCH_POS = "afterpaytouch_pos";
+
     const RATEPAY_PAYMENT_METHOD = 'ratepay';
     const RATEPAY_DIRECTDEBIT_PAYMENT_METHOD = 'ratepay_directdebit';
 
@@ -39,10 +46,26 @@ class OpenInvoice
     const KLARNA_PAYNOW_PAYMENT_METHOD = 'klarna_paynow';
     const KLARNA_ACCOUNT_PAYMENT_METHOD = 'klarna_account';
 
-    const FACILYPAY = 'facilypay_';
-    const AFFIRM ='affirm';
-    const CLEARPAY = 'clearpay';
+    const FACILYPAY = 'facilypay';
+    const FACILYPAY_10X = 'facilypay_10x';
+    const FACILYPAY_10X_MERCHANT_PAYS = 'facilypay_10x_merchant_pays';
+    const FACILYPAY_10X_WITHFEES = 'facilypay_10x_withfees';
+    const FACILYPAY_12X = 'facilypay_12x';
+    const FACILYPAY_12X_MERCHANT_PAYS = 'facilypay_12x_merchant_pays';
+    const FACILYPAY_12X_WITHFEES = 'facilypay_12x_withfees';
+    const FACILYPAY_3X = 'facilypay_3x';
+    const FACILYPAY_3X_MERCHANT_PAYS = 'facilypay_3x_merchant_pays';
+    const FACILYPAY_3X_WITHFEES = 'facilypay_3x_withfees';
+    const FACILYPAY_4X = 'facilypay_4x';
+    const FACILYPAY_4X_MERCHANT_PAYS = 'facilypay_4x_merchant_pays';
+    const FACILYPAY_4X_WITHFEES = 'facilypay_4x_withfees';
+    const FACILYPAY_6X = 'facilypay_6x';
+    const FACILYPAY_6X_MERCHANT_PAYS = 'facilypay_6x_merchant_pays';
+    const FACILYPAY_6X_WITHFEES = 'facilypay_6x_withfees';
+    const FACILYPAY_FR = 'facilypay_fr';
 
+    const AFFIRM = 'affirm';
+    const CLEARPAY = 'clearpay';
 
     /**
      * List of open invoice payment methods
@@ -63,8 +86,39 @@ class OpenInvoice
         self::KLARNA_ACCOUNT_PAYMENT_METHOD,
         self::AFFIRM,
         self::CLEARPAY,
-        self::FACILYPAY
+        self::FACILYPAY,
+        self::FACILYPAY_10X,
+        self::FACILYPAY_10X_MERCHANT_PAYS,
+        self::FACILYPAY_10X_WITHFEES,
+        self::FACILYPAY_12X,
+        self::FACILYPAY_12X_MERCHANT_PAYS,
+        self::FACILYPAY_12X_WITHFEES,
+        self::FACILYPAY_3X,
+        self::FACILYPAY_3X_MERCHANT_PAYS,
+        self::FACILYPAY_3X_WITHFEES,
+        self::FACILYPAY_4X,
+        self::FACILYPAY_4X_MERCHANT_PAYS,
+        self::FACILYPAY_4X_WITHFEES,
+        self::FACILYPAY_6X,
+        self::FACILYPAY_6X_MERCHANT_PAYS,
+        self::FACILYPAY_6X_WITHFEES,
+        self::FACILYPAY_FR,
     );
+
+    /**
+     * List of after pay touch payment methods
+     *
+     * @var array
+     */
+    private static $afterPayTouchPaymentMethods = array(
+        self::AFTERPAYTOUCH_PAYMENT_METHOD,
+        self::AFTERPAYTOUCH_AU,
+        self::AFTERPAYTOUCH_CA,
+        self::AFTERPAYTOUCH_NZ,
+        self::AFTERPAYTOUCH_US,
+        self::AFTERPAYTOUCH_POS
+    );
+
     /**
      * For Klarna And AfterPay use Vat category High others use none
      *
@@ -102,7 +156,8 @@ class OpenInvoice
      */
     public function isAfterPayTouchPaymentMethod($paymentMethod)
     {
-        if (self::AFTERPAYTOUCH_PAYMENT_METHOD === $paymentMethod) {
+        print $paymentMethod;
+        if (in_array($paymentMethod, self::$afterPayTouchPaymentMethods)) {
             return true;
         }
         return false;
