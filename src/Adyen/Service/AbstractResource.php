47bf4b95cc1fd2521acc701281d69b6a21a032d5
Viktor Szépe <viktor@szepe.net>
2021-06-04 16:03:16 +0000

    preg_match's okay is 1

--- src/Adyen/Service/AbstractResource.php
+++ src/Adyen/Service/AbstractResource.php
@@ -209,6 +209,6 @@ abstract class AbstractResource
      */
     private function isBase64Encoded($data)
     {
-        return preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $data) && !empty($data);
+        return preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $data) === 1 && !empty($data);
     }
 }
