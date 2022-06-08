a3511d4df4a45208665c29180e6bccfd3ec7abce
Can Demiralp <ecandemiralp@gmail.com>
2022-06-08 15:13:40 +0200

    [PW-6776] - Remove redundant variable declaration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -39,12 +39,12 @@ class TestCase extends \PHPUnit\Framework\TestCase
      *
      * @var array
      */
-    protected $getSettings;
+    protected $settings;
 
     public function __construct($name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
-        $this->getSettings = $this->loadConfig();
+        $this->settings = $this->loadConfig();
         $this->merchantAccount = $this->getMerchantAccount();
         $this->donationAccount = $this->getDonationAccount();
         $this->skinCode = $this->getSkinCode();
@@ -72,20 +72,17 @@ class TestCase extends \PHPUnit\Framework\TestCase
      */
     protected function createClient()
     {
-        // load settings from .ini file
-        $settings = $this->getSettings;
-
         // validate username, password and MERCHANTAccount
 
-        if (!empty($settings['username']) &&
-            $settings['username'] != "YOUR USERNAME" &&
-            !empty($settings['password']) &&
-            $settings['password'] != "YOUR PASSWORD"
+        if (!empty($this->settings['username']) &&
+            $this->settings['username'] != "YOUR USERNAME" &&
+            !empty($this->settings['password']) &&
+            $this->settings['password'] != "YOUR PASSWORD"
         ) {
             $client = new Client();
             $client->setApplicationName("My Test Application");
-            $client->setUsername($settings['username']);
-            $client->setPassword($settings['password']);
+            $client->setUsername($this->settings['username']);
+            $client->setPassword($this->settings['password']);
             $client->setEnvironment(Environment::TEST);
 
             return $client;
@@ -119,16 +116,13 @@ class TestCase extends \PHPUnit\Framework\TestCase
      */
     protected function createPayoutClient()
     {
-        // load settings from .ini file
-        $settings = $this->getSettings;
-
         // validate username, password and MERCHANTAccount
 
-        if (isset($settings['storePayoutUsername']) && isset($settings['storePayoutPassword'])) {
-            if ($settings['storePayoutUsername'] == "YOUR STORE PAYOUT USERNAME"
-                || $settings['storePayoutUsername'] == ""
-                || $settings['storePayoutPassword'] == "YOUR STORE PAYOUT PASSWORD"
-                || $settings['storePayoutPassword'] == "") {
+        if (isset($this->settings['storePayoutUsername']) && isset($this->settings['storePayoutPassword'])) {
+            if ($this->settings['storePayoutUsername'] == "YOUR STORE PAYOUT USERNAME"
+                || $this->settings['storePayoutUsername'] == ""
+                || $this->settings['storePayoutPassword'] == "YOUR STORE PAYOUT PASSWORD"
+                || $this->settings['storePayoutPassword'] == "") {
                 $client = new Client();
                 $client->setApplicationName("My Test Application");
                 $client->setEnvironment(Environment::TEST);
@@ -139,8 +133,8 @@ class TestCase extends \PHPUnit\Framework\TestCase
             } else {
                 $client = new Client();
                 $client->setApplicationName("My Test Application");
-                $client->setUsername($settings['storePayoutUsername']);
-                $client->setPassword($settings['storePayoutPassword']);
+                $client->setUsername($this->settings['storePayoutUsername']);
+                $client->setPassword($this->settings['storePayoutPassword']);
                 $client->setEnvironment(Environment::TEST);
                 return $client;
             }
@@ -156,16 +150,13 @@ class TestCase extends \PHPUnit\Framework\TestCase
      */
     protected function createReviewPayoutClient()
     {
-        // load settings from .ini file
-        $settings = $this->getSettings;
-
         // validate username, password and MERCHANTAccount
 
-        if (isset($settings['reviewPayoutUsername']) && isset($settings['reviewPayoutPassword'])) {
-            if ($settings['reviewPayoutUsername'] == "YOUR REVIEW PAYOUT USERNAME"
-                || $settings['reviewPayoutUsername'] == ""
-                || $settings['reviewPayoutPassword'] == "YOUR REVIEW PAYOUT PASSWORD"
-                || $settings['reviewPayoutPassword'] == "") {
+        if (isset($this->settings['reviewPayoutUsername']) && isset($this->settings['reviewPayoutPassword'])) {
+            if ($this->settings['reviewPayoutUsername'] == "YOUR REVIEW PAYOUT USERNAME"
+                || $this->settings['reviewPayoutUsername'] == ""
+                || $this->settings['reviewPayoutPassword'] == "YOUR REVIEW PAYOUT PASSWORD"
+                || $this->settings['reviewPayoutPassword'] == "") {
                 $client = new Client();
                 $client->setApplicationName("My Test Application");
                 $client->setEnvironment(Environment::TEST);
@@ -176,8 +167,8 @@ class TestCase extends \PHPUnit\Framework\TestCase
             } else {
                 $client = new Client();
                 $client->setApplicationName("My Test Application");
-                $client->setUsername($settings['reviewPayoutUsername']);
-                $client->setPassword($settings['reviewPayoutPassword']);
+                $client->setUsername($this->settings['reviewPayoutUsername']);
+                $client->setPassword($this->settings['reviewPayoutPassword']);
                 $client->setEnvironment(Environment::TEST);
                 return $client;
             }
@@ -190,16 +181,13 @@ class TestCase extends \PHPUnit\Framework\TestCase
 
     protected function createTerminalCloudAPIClient()
     {
-        // load settings from .ini file
-        $settings = $this->getSettings;
-
-        if (empty($settings['x-api-key']) || $settings['x-api-key'] == 'YOUR X-API KEY') {
+        if (empty($this->settings['x-api-key']) || $this->settings['x-api-key'] == 'YOUR X-API KEY') {
             $this->skipTest("Skipped the test. Configure your x-api-key in the config");
         } else {
             $client = new Client();
             $client->setApplicationName("My Test Terminal API App");
             $client->setEnvironment(Environment::TEST);
-            $client->setXApiKey($settings['x-api-key']);
+            $client->setXApiKey($this->settings['x-api-key']);
             return $client;
         }
     }
@@ -208,15 +196,12 @@ class TestCase extends \PHPUnit\Framework\TestCase
     {
         $client = $this->createClient();
 
-        // load settings from .ini file
-        $settings = $this->getSettings;
-
-        if (empty($settings['merchantAccount']) || $settings['merchantAccount'] == 'YOUR MERCHANTACCOUNT') {
+        if (empty($this->settings['merchantAccount']) || $this->settings['merchantAccount'] == 'YOUR MERCHANTACCOUNT') {
             $this->skipTest("Skipped the test. Configure your MerchantAccount in the config");
             return null;
         }
 
-        $client->setMerchantAccount($settings['merchantAccount']);
+        $client->setMerchantAccount($this->settings['merchantAccount']);
         return $client;
     }
 
@@ -224,70 +209,57 @@ class TestCase extends \PHPUnit\Framework\TestCase
     {
         $client = $this->createClientWithMerchantAccount();
 
-        // load settings from .ini file
-        $settings = $this->getSettings;
-
-        if (empty($settings['x-api-key']) || $settings['x-api-key'] == 'YOUR X-API KEY') {
+        if (empty($this->settings['x-api-key']) || $this->settings['x-api-key'] == 'YOUR X-API KEY') {
             $this->skipTest("Skipped the test. Configure your x-api-key");
         } else {
-            $client->setXApiKey($settings['x-api-key']);
+            $client->setXApiKey($this->settings['x-api-key']);
             return $client;
         }
     }
 
     protected function getMerchantAccount()
     {
-        $settings = $this->getSettings;
-
-        if (empty($settings['merchantAccount']) || $settings['merchantAccount'] == 'YOUR MERCHANTACCOUNT') {
+        if (empty($this->settings['merchantAccount']) || $this->settings['merchantAccount'] == 'YOUR MERCHANTACCOUNT') {
             return null;
         }
 
-        return $settings['merchantAccount'];
+        return $this->settings['merchantAccount'];
     }
 
     protected function getDonationAccount()
     {
-        $settings = $this->getSettings;
-
-        if (empty($settings['donationAccount']) || $settings['donationAccount'] == 'YOUR DONATION MERCHANT ACCOUNT') {
+        if (empty($this->settings['donationAccount']) || $this->settings['donationAccount'] == 'YOUR DONATION MERCHANT ACCOUNT') {
             return null;
         }
 
-        return $settings['donationAccount'];
+        return $this->settings['donationAccount'];
     }
 
     protected function getSkinCode()
     {
-        $settings = $this->getSettings;
-
-        if (empty($settings['skinCode']) || $settings['skinCode'] == 'YOUR SKIN CODE') {
+        if (empty($this->settings['skinCode']) || $this->settings['skinCode'] == 'YOUR SKIN CODE') {
             return null;
         }
 
-        return $settings['skinCode'];
+        return $this->settings['skinCode'];
     }
 
     protected function getHmacSignature()
     {
-        $settings = $this->getSettings;
-
-        if (empty($settings['hmacSignature'])|| $settings['hmacSignature'] == 'YOUR HMAC SIGNATURE') {
+        if (empty($this->settings['hmacSignature'])|| $this->settings['hmacSignature'] == 'YOUR HMAC SIGNATURE') {
             return null;
         }
 
-        return $settings['hmacSignature'];
+        return $this->settings['hmacSignature'];
     }
 
     protected function getPOIID()
     {
-        $settings = $this->getSettings;
-
-        if (empty($settings['POIID']) || $settings['POIID'] == 'MODEL-SERIALNUMBER') {
+        if (empty($this->settings['POIID']) || $this->settings['POIID'] == 'MODEL-SERIALNUMBER') {
             return null;
         }
 
-        return $settings['POIID'];
+        return $this->settings['POIID'];
     }
 
     protected function loadConfig()
