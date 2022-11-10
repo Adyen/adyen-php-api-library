41e57d7d1f6b30e832fe2c8b261b4bf57b458a16
Guillaume Ferrand <guillaume@obypay.com>
2022-11-10 11:49:33 +0100

    back to standard http request

--- src/Adyen/Service/ResourceModel/Management/StoreAccount.php
+++ src/Adyen/Service/ResourceModel/Management/StoreAccount.php
@@ -6,11 +6,6 @@ namespace Adyen\Service\ResourceModel\Management;
 class StoreAccount extends \Adyen\Service\AbstractResource
 {
     /**
-     * @var string
-     */
-    protected $endpoint;
-
-    /**
      * Include applicationInfo key in the request parameters
      *
      * @var bool
@@ -22,12 +17,10 @@ class StoreAccount extends \Adyen\Service\AbstractResource
      * @return mixed
      * @throws \Adyen\AdyenException
      */
-    public function list($service)
+    public function list(array $queryParams = [])
     {
-        // $url = $this->managementEndpoint . "/stores";
-        // return $this->requestHttp($url, 'get', $queryParams);
-        $this->endpoint = $this->managementEndpoint . "/stores";
-        parent::__construct($service, $this->endpoint, $this->allowApplicationInfo);
+        $url = $this->managementEndpoint . "/stores";
+        return $this->requestHttp($url, 'get', $queryParams);
     }
 
     /**
