f260790f790773136e422b3f519497544a6d5ba1
jillingk <93914435+jillingk@users.noreply.github.com>
2023-03-29 10:13:31 +0200

    fix unused var in test

--- tests/Unit/ServiceTest.php
+++ tests/Unit/ServiceTest.php
@@ -3,39 +3,44 @@
 namespace Adyen\Tests\Unit;
 
 use Adyen\AdyenException;
+use Adyen\Client;
 use Adyen\Environment;
 use Adyen\Service;
-use http\Client;
-use mysql_xdevapi\Exception;
 use function PHPUnit\Framework\assertEquals;
 
 class ServiceTest extends TestCaseMock
 {
     public function testLiveURLPrefixPal()
     {
-        $client = new \Adyen\Client();
+        $client = new Client();
         $client->setEnvironment(Environment::LIVE, "[prefix]");
         $service = new Service($client);
         $url = $service->createBaseUrl("https://pal-test.adyen.com/pal/servlet/Payment/[version]/[method]");
-        self::assertEquals("https://[prefix]-pal-live.adyenpayments.com/pal/servlet/Payment/[version]/[method]", $url);
+        self::assertEquals(
+            "https://[prefix]-pal-live.adyenpayments.com/pal/servlet/Payment/[version]/[method]",
+            $url
+        );
     }
 
     public function testLiveURLPrefixCheckout()
     {
-        $client = new \Adyen\Client();
+        $client = new Client();
         $client->setEnvironment(Environment::LIVE, "[prefix]");
         $service = new Service($client);
         $url = $service->createBaseUrl("https://checkout-test.adyen.com/[version]/[method]");
-        self::assertEquals("https://[prefix]-checkout-live.adyenpayments.com/checkout/[version]/[method]", $url);
+        self::assertEquals(
+            "https://[prefix]-checkout-live.adyenpayments.com/checkout/[version]/[method]",
+            $url
+        );
     }
 
     public function testLiveEndpointWithoutRequiredPrefix()
     {
-        $client = new \Adyen\Client();
+        $client = new Client();
         $client->setEnvironment(Environment::LIVE);
         $service = new Service($client);
         try {
-            $url = $service->createBaseUrl("https://checkout-test.adyen.com/[version]/[method]");
+            $service->createBaseUrl("https://checkout-test.adyen.com/[version]/[method]");
         } catch (AdyenException $e) {
             assertEquals(
                 "Please add your checkout live URL prefix from CA under Developers > API URLs > Prefix",
@@ -46,7 +51,7 @@ class ServiceTest extends TestCaseMock
 
     public function testLiveURLPrefixOther()
     {
-        $client = new \Adyen\Client();
+        $client = new Client();
         $client->setEnvironment(Environment::LIVE);
         $service = new Service($client);
         $url = $service->createBaseUrl("https://kyc-test.adyen.com/lem/v3/legalEntities");
