262bb9671124f390a6d8cff4ab9f1c48c61278c4
jillingk <93914435+jillingk@users.noreply.github.com>
2023-04-04 16:01:59 +0200

    generate Recurring and create UT

---
+++ tests/Unit/ModelBasedRecurringTest.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen API Library for PHP
+ *
+ * Copyright (c) 2021 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ */
+
+namespace Adyen\Tests\Unit;
+
+use Adyen\Model\Recurring\NotifyShopperRequest;
+use Adyen\Model\Recurring\ScheduleAccountUpdaterRequest;
+
+class ModelBasedRecurringTest extends TestCaseMock
+{
+    /**
+     * @param $jsonFile
+     * @param $httpStatus
+     * @dataProvider notifyShopperDataProvider
+     * @throws \Adyen\AdyenException
+     */
+    public function testNotifyShopperSuccess($jsonFile, $httpStatus)
+    {
+        $client = $this->createMockClient($jsonFile, $httpStatus);
+        $service = new \Adyen\Service\RecurringApi($client);
+
+        $result = $service->notifyShopper(new NotifyShopperRequest());
+        $this->assertEquals('Example displayed reference', $result->getDisplayedReference());
+        $this->assertEquals('Request processed successfully', $result->getMessage());
+        $this->assertEquals('8516167336214570', $result->getPspReference());
+        $this->assertEquals('Success', $result->getResultCode());
+        $this->assertEquals('IA0F7500002462', $result->getShopperNotificationReference());
+    }
+
+    public static function notifyShopperDataProvider()
+    {
+        return array(
+            array('tests/Resources/Recurring/notifyShopper-success.json', 200)
+        );
+    }
+
+    /**
+     * @param $jsonFile
+     * @param $httpStatus
+     * @dataProvider scheduleAccountUpdaterDataProvider
+     * @throws \Adyen\AdyenException
+     */
+    public function testScheduleAccountUpdaterSuccess($jsonFile, $httpStatus)
+    {
+        $client = $this->createMockClient($jsonFile, $httpStatus);
+        $service = new \Adyen\Service\RecurringApi($client);
+
+        $result = $service->scheduleAccountUpdater(new ScheduleAccountUpdaterRequest());
+        $this->assertEquals('8516167336214570', $result->getPspReference());
+        $this->assertEquals('Success', $result->getResult());
+    }
+
+    public static function scheduleAccountUpdaterDataProvider()
+    {
+        return array(
+            array('tests/Resources/Recurring/scheduleAccountUpdater-success.json', 200)
+        );
+    }
+}
