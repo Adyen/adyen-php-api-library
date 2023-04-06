63bcb75b240222cc524ced51fc2a0b605b581a97
michaelpa <michael.paul@adyen.com>
2023-04-06 16:27:50 +0200

    ITT-413: Remove contact info

--- tests/Unit/DataProtectionTest.php
+++ tests/Unit/DataProtectionTest.php
@@ -3,6 +3,10 @@
 namespace Adyen\Tests\Unit;
 
 use Adyen\AdyenException;
+use Adyen\Client;
+use Adyen\Config;
+use Adyen\Environment;
+use Adyen\HttpClient\ClientInterface;
 use Adyen\Model\DataProtection\SubjectErasureByPspReferenceRequest;
 use Adyen\Model\DataProtection\SubjectErasureResponse;
 use Adyen\Service\DataProtectionApi;
@@ -12,7 +16,7 @@ class DataProtectionTest extends TestCaseMock
     /**
      * @throws AdyenException
      */
-    public function testRequestSubjectErasure()
+    public function testRequestSubjectErasureSuccess()
     {
         $client = $this->createMockClient(
             'tests/Resources/DataProtection/post-request-subject-erasure-success.json',
@@ -27,5 +31,77 @@ class DataProtectionTest extends TestCaseMock
         $response = $service->requestSubjectErasure($request);
 
         $this->assertSame(SubjectErasureResponse::RESULT_SUCCESS, $response->getResult());
+    }
+
+    public function testRequestSubjectErasureAuthorizationError()
+    {
+        $this->expectException(AdyenException::class);
+        $this->expectExceptionCode(403);
+        $this->expectExceptionMessage('Not allowed');
+        $client = $this->createMockClient(
+            'tests/Resources/DataProtection/authorization-error.json',
+            403
+        );
+        $service = new DataProtectionApi($client);
+        $request = new SubjectErasureByPspReferenceRequest();
+        $request->setMerchantAccount("NOT_YOUR_MERCHANT_ACCOUNT");
+        $request->setPspReference("9915520502347613");
+
+        $service->requestSubjectErasure($request);
+    }
+
+    /**
+     * @throws AdyenException
+     */
+    public function testRequestSubjectErasureTestEndpoint()
+    {
+        $client = new Client(new Config());
+        $client->setEnvironment(Environment::TEST);
+        $http = $this->createMock(ClientInterface::class);
+        $client->setHttpClient($http);
+        $service = new DataProtectionApi($client);
+        $http->expects($this->once())
+            ->method('requestHttp')
+            ->with(
+                $service,
+                'https://ca-test.adyen.com/ca/services/DataProtectionService/v1/requestSubjectErasure',
+                [
+                    'merchantAccount' => "YOUR_MERCHANT_ACCOUNT",
+                    'forceErasure' => false
+                ],
+                'post',
+                null
+            )
+            ->willReturn([]);
+        $request = new SubjectErasureByPspReferenceRequest();
+        $request->setMerchantAccount("YOUR_MERCHANT_ACCOUNT");
+        $request->setForceErasure(false);
+
+        $service->requestSubjectErasure($request);
+    }
+
+    /**
+     * @throws AdyenException
+     */
+    public function testRequestSubjectErasureLiveEndpoint()
+    {
+        $client = new Client(new Config());
+        $client->setEnvironment(Environment::LIVE);
+        $http = $this->createMock(ClientInterface::class);
+        $client->setHttpClient($http);
+        $service = new DataProtectionApi($client);
+        $http->expects($this->once())
+            ->method('requestHttp')
+            ->with(
+                $service,
+                'https://ca-live.adyen.com/ca/services/DataProtectionService/v1/requestSubjectErasure',
+                [],
+                'post',
+                null
+            )
+            ->willReturn([]);
+        $request = new SubjectErasureByPspReferenceRequest();
+
+        $service->requestSubjectErasure($request);
     }
 }
