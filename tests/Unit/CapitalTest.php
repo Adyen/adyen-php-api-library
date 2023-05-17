060ef8c41596e1fabdb314957f9d5d465d6d913c
Antoni Stroinski <55943882+antolo-arch@users.noreply.github.com>
2023-05-17 14:55:02 +0000

    generate capital service

---
+++ tests/Unit/CapitalTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Adyen\Tests\Unit;
+
+class CapitalTest extends TestCaseMock
+{
+    public function testGetCapitalAccount()
+    {
+        // create Checkout client
+        $client = $this->createMockClientUrl('tests/Resources/Capital/get-capital-account.json');
+
+        // initialize service
+        $service = new \Adyen\Service\CapitalApi($client);
+
+        $result = $service->getCapitalAccount();
+        $this->assertEquals(0, $result->getGrants()[0]->getAmount()->getValue());
+    }
+
+
+}
