2f9a709e0ed9ef06ff59bf35fe0672125cda1e5a
jillingk <93914435+jillingk@users.noreply.github.com>
2023-08-25 16:54:12 +0200

    rebase develop

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ openapi-generator-jar:=target/openapi-generator-cli.jar
 openapi-generator-cli:=java -jar $(openapi-generator-jar)
 
 generator:=php
-modelGen:=AcsWebhooks BalanceControl BalancePlatform Checkout ConfigurationWebhooks StoredValue Payments Payout Management LegalEntityManagement TransferWebhooks Transfers BinLookup StoredValue POSTerminalManagement Recurring ReportWebhooks
+modelGen:=AcsWebhooks BalanceControl BalancePlatform Checkout ConfigurationWebhooks StoredValue Payments Payout Management ManagementWebhooks LegalEntityManagement TransferWebhooks Transfers BinLookup StoredValue POSTerminalManagement Recurring ReportWebhooks
 models:=src/Adyen/Model
 output:=target/out
 
@@ -30,6 +30,8 @@ AcsWebhooks: spec=BalancePlatformAcsNotification-v1
 ConfigurationWebhooks: spec=BalancePlatformConfigurationNotification-v1
 ReportWebhooks: spec=BalancePlatformReportNotification-v1
 TransferWebhooks: spec=BalancePlatformTransferNotification-v3
+# ManagementWebhooks
+ManagementWebhooks: spec=ManagementNotificationService-v1
 
 # Classic Platforms
 marketpay/account: spec=AccountService-v6
--- tests/Unit/NotificationTest.php
+++ tests/Unit/NotificationTest.php
@@ -27,7 +27,9 @@ use Adyen\Model\AcsWebhooks\AuthenticationNotificationRequest;
 use Adyen\Model\BalancePlatform\Balance;
 use Adyen\Model\ConfigurationWebhooks\BalanceAccountNotificationRequest;
 use Adyen\Model\ConfigurationWebhooks\SweepConfigurationNotificationRequest;
+use Adyen\Model\ManagementWebhooks\PaymentMethodCreatedNotificationRequest;
 use Adyen\Service\BankingWebhookParser;
+use Adyen\Service\ManagementWebhookParser;
 use Adyen\Service\Notification;
 
 class NotificationTest extends TestCaseMock
@@ -276,6 +278,27 @@ class NotificationTest extends TestCaseMock
         $result = $webhookParser->getGenericWebhook();
         self::assertEquals(SweepConfigurationNotificationRequest::class, get_class($result));
         self::assertEquals("test", $result->getEnvironment());
+    }
+
+    public function testManagementWebhookParser()
+    {
+        $jsonString = '{
+          "createdAt": "2022-01-24T14:59:11+01:00",
+          "data": {
+            "id": "PM3224R223224K5FH4M2K9B86",
+            "merchantId": "MERCHANT_ACCOUNT",
+            "result": "SUCCESS",
+            "storeId": "ST322LJ223223K5F4SQNR9XL5",
+            "type": "visa"
+          },
+          "environment": "test",
+          "type": "paymentMethod.created"
+        }';
+        $webhookParser = new ManagementWebhookParser($jsonString);
+        $result = $webhookParser->getPaymentMethodCreatedNotificationRequest();
+        self::assertEquals(PaymentMethodCreatedNotificationRequest::class, get_class($result));
+        $paymentWebhook = new PaymentMethodCreatedNotificationRequest();
+        self::assertEquals($paymentWebhook->getTypeAllowableValues()[0], $result->getType());
     }
 
     public function testBankingWebhookParserAuthenticationRequest()
