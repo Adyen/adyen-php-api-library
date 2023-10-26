68875247ae3bbb62553369c0145f378738c280fa
Alessio Zampatti <alessio.zampatti@adyen.com>
2023-10-26 13:50:50 +0000

    fix codesniffer

--- src/Adyen/Model/Checkout/CheckoutPaymentMethod.php
+++ src/Adyen/Model/Checkout/CheckoutPaymentMethod.php
@@ -1644,7 +1644,7 @@ class CheckoutPaymentMethod implements ModelInterface, ArrayAccess, \JsonSeriali
     /**
      * Sets shopperEmail
      *
-     * @param string $shopperEmail 
+     * @param string $shopperEmail
      *
      * @return self
      */
@@ -1671,7 +1671,7 @@ class CheckoutPaymentMethod implements ModelInterface, ArrayAccess, \JsonSeriali
     /**
      * Sets telephoneNumber
      *
-     * @param string $telephoneNumber 
+     * @param string $telephoneNumber
      *
      * @return self
      */
