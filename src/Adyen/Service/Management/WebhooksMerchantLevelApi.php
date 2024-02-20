18386dae36c09c266c001c669d201326a5653767
AdyenAutomationBot <Adyen Automation plugins_dev@adyen.com>
2024-02-20 10:40:49 +0000

    style(fmt): code formatted

--- src/Adyen/Service/Management/WebhooksMerchantLevelApi.php
+++ src/Adyen/Service/Management/WebhooksMerchantLevelApi.php
@@ -92,14 +92,13 @@ class WebhooksMerchantLevelApi extends Service
     * @param string $merchantId
     * @param string $webhookId
     * @param array|null $requestOptions
-    
+
     * @throws AdyenException
     */
     public function removeWebhook(string $merchantId, string $webhookId, array $requestOptions = null)
     {
         $endpoint = $this->baseURL . str_replace(['{merchantId}', '{webhookId}'], [$merchantId, $webhookId], "/merchants/{merchantId}/webhooks/{webhookId}");
         $this->requestHttp($endpoint, strtolower('DELETE'), null, $requestOptions);
-        
     }
 
     /**
