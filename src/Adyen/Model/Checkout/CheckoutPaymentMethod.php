b48d1c0280815b022bbe89e302c2554214a91fa5
Adyen Automation <plugins_dev@adyen.com>
2024-03-28 14:10:38 +0000

    [reformat][adyen-sdk-automation] automated change

--- src/Adyen/Model/Checkout/CheckoutPaymentMethod.php
+++ src/Adyen/Model/Checkout/CheckoutPaymentMethod.php
@@ -1682,7 +1682,7 @@ class CheckoutPaymentMethod implements ModelInterface, ArrayAccess, \JsonSeriali
     /**
      * Sets shopperEmail
      *
-     * @param string $shopperEmail
+     * @param string $shopperEmail 
      *
      * @return self
      */
@@ -1709,7 +1709,7 @@ class CheckoutPaymentMethod implements ModelInterface, ArrayAccess, \JsonSeriali
     /**
      * Sets telephoneNumber
      *
-     * @param string $telephoneNumber
+     * @param string $telephoneNumber 
      *
      * @return self
      */
