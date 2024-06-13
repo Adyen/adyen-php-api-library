90ab284fdb3d9aaaf96833af7d148e4af95ebd7f
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2024-06-13 09:07:21 +0200

    Update all services (#663)
    * [reformat][adyen-sdk-automation] automated change
    
    * style(fmt): code formatted
    
    ---------
    
    Co-authored-by: AdyenAutomationBot <Adyen Automation plugins_dev@adyen.com>

--- src/Adyen/Model/Management/AndroidAppError.php
+++ src/Adyen/Model/Management/AndroidAppError.php
@@ -19,7 +19,7 @@ use \ArrayAccess;
 use Adyen\Model\Management\ObjectSerializer;
 
 /**
- * AndroidApp Class Doc Comment
+ * AndroidAppError Class Doc Comment
  *
  * @category Class
  * @package  Adyen
@@ -27,7 +27,7 @@ use Adyen\Model\Management\ObjectSerializer;
  * @link     https://openapi-generator.tech
  * @implements \ArrayAccess<string, mixed>
  */
-class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
+class AndroidAppError implements ModelInterface, ArrayAccess, \JsonSerializable
 {
     public const DISCRIMINATOR = null;
 
@@ -36,7 +36,7 @@ class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
       *
       * @var string
       */
-    protected static $openAPIModelName = 'AndroidApp';
+    protected static $openAPIModelName = 'AndroidAppError';
 
     /**
       * Array of property to type mappings. Used for (de)serialization
@@ -44,15 +44,8 @@ class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
       * @var string[]
       */
     protected static $openAPITypes = [
-        'description' => 'string',
         'errorCode' => 'string',
-        'errors' => '\Adyen\Model\Management\AndroidAppError[]',
-        'id' => 'string',
-        'label' => 'string',
-        'packageName' => 'string',
-        'status' => 'string',
-        'versionCode' => 'int',
-        'versionName' => 'string'
+        'terminalModels' => 'string[]'
     ];
 
     /**
@@ -63,15 +56,8 @@ class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
       * @psalm-var array<string, string|null>
       */
     protected static $openAPIFormats = [
-        'description' => null,
         'errorCode' => null,
-        'errors' => null,
-        'id' => null,
-        'label' => null,
-        'packageName' => null,
-        'status' => null,
-        'versionCode' => 'int32',
-        'versionName' => null
+        'terminalModels' => null
     ];
 
     /**
@@ -80,15 +66,8 @@ class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
       * @var boolean[]
       */
     protected static $openAPINullables = [
-        'description' => false,
         'errorCode' => false,
-        'errors' => false,
-        'id' => false,
-        'label' => false,
-        'packageName' => false,
-        'status' => false,
-        'versionCode' => true,
-        'versionName' => false
+        'terminalModels' => false
     ];
 
     /**
@@ -177,15 +156,8 @@ class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
      * @var string[]
      */
     protected static $attributeMap = [
-        'description' => 'description',
         'errorCode' => 'errorCode',
-        'errors' => 'errors',
-        'id' => 'id',
-        'label' => 'label',
-        'packageName' => 'packageName',
-        'status' => 'status',
-        'versionCode' => 'versionCode',
-        'versionName' => 'versionName'
+        'terminalModels' => 'terminalModels'
     ];
 
     /**
@@ -194,15 +166,8 @@ class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
      * @var string[]
      */
     protected static $setters = [
-        'description' => 'setDescription',
         'errorCode' => 'setErrorCode',
-        'errors' => 'setErrors',
-        'id' => 'setId',
-        'label' => 'setLabel',
-        'packageName' => 'setPackageName',
-        'status' => 'setStatus',
-        'versionCode' => 'setVersionCode',
-        'versionName' => 'setVersionName'
+        'terminalModels' => 'setTerminalModels'
     ];
 
     /**
@@ -211,15 +176,8 @@ class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
      * @var string[]
      */
     protected static $getters = [
-        'description' => 'getDescription',
         'errorCode' => 'getErrorCode',
-        'errors' => 'getErrors',
-        'id' => 'getId',
-        'label' => 'getLabel',
-        'packageName' => 'getPackageName',
-        'status' => 'getStatus',
-        'versionCode' => 'getVersionCode',
-        'versionName' => 'getVersionName'
+        'terminalModels' => 'getTerminalModels'
     ];
 
     /**
@@ -279,15 +237,8 @@ class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
      */
     public function __construct(array $data = null)
     {
-        $this->setIfExists('description', $data ?? [], null);
         $this->setIfExists('errorCode', $data ?? [], null);
-        $this->setIfExists('errors', $data ?? [], null);
-        $this->setIfExists('id', $data ?? [], null);
-        $this->setIfExists('label', $data ?? [], null);
-        $this->setIfExists('packageName', $data ?? [], null);
-        $this->setIfExists('status', $data ?? [], null);
-        $this->setIfExists('versionCode', $data ?? [], null);
-        $this->setIfExists('versionName', $data ?? [], null);
+        $this->setIfExists('terminalModels', $data ?? [], null);
     }
 
     /**
@@ -317,12 +268,6 @@ class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
     {
         $invalidProperties = [];
 
-        if ($this->container['id'] === null) {
-            $invalidProperties[] = "'id' can't be null";
-        }
-        if ($this->container['status'] === null) {
-            $invalidProperties[] = "'status' can't be null";
-        }
         return $invalidProperties;
     }
 
@@ -339,37 +284,9 @@ class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
 
 
     /**
-     * Gets description
-     *
-     * @return string|null
-     */
-    public function getDescription()
-    {
-        return $this->container['description'];
-    }
-
-    /**
-     * Sets description
-     *
-     * @param string|null $description The description that was provided when uploading the app. The description is not shown on the terminal.
-     *
-     * @return self
-     */
-    public function setDescription($description)
-    {
-        if (is_null($description)) {
-            throw new \InvalidArgumentException('non-nullable description cannot be null');
-        }
-        $this->container['description'] = $description;
-
-        return $this;
-    }
-
-    /**
      * Gets errorCode
      *
      * @return string|null
-     * @deprecated
      */
     public function getErrorCode()
     {
@@ -382,7 +299,6 @@ class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
      * @param string|null $errorCode The error code of the Android app with the `status` of either **error** or **invalid**.
      *
      * @return self
-     * @deprecated
      */
     public function setErrorCode($errorCode)
     {
@@ -395,188 +311,28 @@ class AndroidApp implements ModelInterface, ArrayAccess, \JsonSerializable
     }
 
     /**
-     * Gets errors
+     * Gets terminalModels
      *
-     * @return \Adyen\Model\Management\AndroidAppError[]|null
+     * @return string[]|null
      */
-    public function getErrors()
+    public function getTerminalModels()
     {
-        return $this->container['errors'];
+        return $this->container['terminalModels'];
     }
 
     /**
-     * Sets errors
+     * Sets terminalModels
      *
-     * @param \Adyen\Model\Management\AndroidAppError[]|null $errors The list of errors of the Android app.
+     * @param string[]|null $terminalModels The list of payment terminal models to which the returned `errorCode` applies.
      *
      * @return self
      */
-    public function setErrors($errors)
+    public function setTerminalModels($terminalModels)
     {
-        if (is_null($errors)) {
-            throw new \InvalidArgumentException('non-nullable errors cannot be null');
+        if (is_null($terminalModels)) {
+            throw new \InvalidArgumentException('non-nullable terminalModels cannot be null');
         }
-        $this->container['errors'] = $errors;
-
-        return $this;
-    }
-
-    /**
-     * Gets id
-     *
-     * @return string
-     */
-    public function getId()
-    {
-        return $this->container['id'];
-    }
-
-    /**
-     * Sets id
-     *
-     * @param string $id The unique identifier of the app.
-     *
-     * @return self
-     */
-    public function setId($id)
-    {
-        if (is_null($id)) {
-            throw new \InvalidArgumentException('non-nullable id cannot be null');
-        }
-        $this->container['id'] = $id;
-
-        return $this;
-    }
-
-    /**
-     * Gets label
-     *
-     * @return string|null
-     */
-    public function getLabel()
-    {
-        return $this->container['label'];
-    }
-
-    /**
-     * Sets label
-     *
-     * @param string|null $label The app name that is shown on the terminal.
-     *
-     * @return self
-     */
-    public function setLabel($label)
-    {
-        if (is_null($label)) {
-            throw new \InvalidArgumentException('non-nullable label cannot be null');
-        }
-        $this->container['label'] = $label;
-
-        return $this;
-    }
-
-    /**
-     * Gets packageName
-     *
-     * @return string|null
-     */
-    public function getPackageName()
-    {
-        return $this->container['packageName'];
-    }
-
-    /**
-     * Sets packageName
-     *
-     * @param string|null $packageName The package name that uniquely identifies the Android app.
-     *
-     * @return self
-     */
-    public function setPackageName($packageName)
-    {
-        if (is_null($packageName)) {
-            throw new \InvalidArgumentException('non-nullable packageName cannot be null');
-        }
-        $this->container['packageName'] = $packageName;
-
-        return $this;
-    }
-
-    /**
-     * Gets status
-     *
-     * @return string
-     */
-    public function getStatus()
-    {
-        return $this->container['status'];
-    }
-
-    /**
-     * Sets status
-     *
-     * @param string $status The status of the app. Possible values:  * `processing`: the app is being signed and converted to a format that the terminal can handle. * `error`: something went wrong. Check that the app matches the [requirements](https://docs.adyen.com/point-of-sale/android-terminals/app-requirements). * `invalid`: there is something wrong with the APK file of the app. * `ready`: the app has been signed and converted. * `archived`: the app is no longer available.
-     *
-     * @return self
-     */
-    public function setStatus($status)
-    {
-        if (is_null($status)) {
-            throw new \InvalidArgumentException('non-nullable status cannot be null');
-        }
-        $this->container['status'] = $status;
-
-        return $this;
-    }
-
-    /**
-     * Gets versionCode
-     *
-     * @return int|null
-     */
-    public function getVersionCode()
-    {
-        return $this->container['versionCode'];
-    }
-
-    /**
-     * Sets versionCode
-     *
-     * @param int|null $versionCode The version number of the app.
-     *
-     * @return self
-     */
-    public function setVersionCode($versionCode)
-    {
-        // Do nothing for nullable integers
-        $this->container['versionCode'] = $versionCode;
-
-        return $this;
-    }
-
-    /**
-     * Gets versionName
-     *
-     * @return string|null
-     */
-    public function getVersionName()
-    {
-        return $this->container['versionName'];
-    }
-
-    /**
-     * Sets versionName
-     *
-     * @param string|null $versionName The app version number that is shown on the terminal.
-     *
-     * @return self
-     */
-    public function setVersionName($versionName)
-    {
-        if (is_null($versionName)) {
-            throw new \InvalidArgumentException('non-nullable versionName cannot be null');
-        }
-        $this->container['versionName'] = $versionName;
+        $this->container['terminalModels'] = $terminalModels;
 
         return $this;
     }
