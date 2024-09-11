c39f5081b0d811989b05156d23084561913f8326
AdyenAutomationBot <Adyen Automation plugins_dev@adyen.com>
2024-09-11 09:42:45 +0000

    style(fmt): code formatted

--- src/Adyen/Model/Checkout/VippsDetails.php
+++ src/Adyen/Model/Checkout/VippsDetails.php
@@ -415,7 +415,7 @@ class VippsDetails implements ModelInterface, ArrayAccess, \JsonSerializable
     /**
      * Sets telephoneNumber
      *
-     * @param string $telephoneNumber 
+     * @param string $telephoneNumber
      *
      * @return self
      */
