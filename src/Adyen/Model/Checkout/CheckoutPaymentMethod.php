fd7d392b2809dd1ce920e0858b930daeda0ccaae
AdyenAutomationBot <Adyen Automation plugins_dev@adyen.com>
2024-09-26 16:28:48 +0000

    style(fmt): code formatted

--- src/Adyen/Model/Checkout/CheckoutPaymentMethod.php
+++ src/Adyen/Model/Checkout/CheckoutPaymentMethod.php
@@ -1922,7 +1922,7 @@ class CheckoutPaymentMethod implements ModelInterface, ArrayAccess, \JsonSeriali
     /**
      * Sets shopperEmail
      *
-     * @param string $shopperEmail 
+     * @param string $shopperEmail
      *
      * @return self
      */
@@ -1946,7 +1946,7 @@ class CheckoutPaymentMethod implements ModelInterface, ArrayAccess, \JsonSeriali
     /**
      * Sets telephoneNumber
      *
-     * @param string $telephoneNumber 
+     * @param string $telephoneNumber
      *
      * @return self
      */
