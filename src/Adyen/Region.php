a573952395a3f427ea9bf8715fcf951375ceaa11
Djoyke Reijans <115019123+DjoykeAbyah@users.noreply.github.com>
2024-12-10 12:49:30 +0100

    added APSE to VALID_REGIONS (#717)
    * added APSE to VALID_REGIONS
    
    * added unit test for valid regions
    
    * adjusted test to assert region and mapping separately, removed client in terminal endpoint from client.php and region.php mapping

--- src/Adyen/Region.php
+++ src/Adyen/Region.php
@@ -4,18 +4,47 @@ namespace Adyen;
 
 class Region
 {
+    /**
+     * European Union region
+    */
     const EU = "eu";
+    
+    /**
+     * United States region
+     */
     const US = "us";
+
+    /**
+     * Australia region
+     */
     const AU = "au";
+    
+    /**
+     * India region
+     */
     const IN = "in";
 
+    /**
+     * Asia-Pacific, South East region
+     */
+    const APSE = "apse";
+
+   /**
+     * List of all valid regions
+     * @var array<int,string>
+     */
     const VALID_REGIONS = [
         self::EU,
         self::US,
         self::AU,
-        self::IN
+        self::IN,
+        self::APSE
     ];
 
+    /**
+     * Maps regions to their respective Terminal API endpoints.
+     * @var array<string, string>
+     */
     const TERMINAL_API_ENDPOINTS_MAPPING = [
         self::EU => Client::ENDPOINT_TERMINAL_CLOUD_LIVE,
         self::US => Client::ENDPOINT_TERMINAL_CLOUD_US_LIVE,
