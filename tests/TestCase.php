8db438039c209c58645b223ac65bc69b68b9d023
Djoyke Reijans <115019123+DjoykeAbyah@users.noreply.github.com>
2025-01-07 09:46:19 +0000

    removed whitespaces

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -405,7 +405,7 @@ class TestCase extends \PHPUnit\Framework\TestCase
 
     /**
      * Ensures an exception is thrown for unsupported region India in LIVE environment
-     * 
+     *
      * @throws \Adyen\AdyenException
      */
     public function testUnmappedIndiaRegionThrowsException(): void
