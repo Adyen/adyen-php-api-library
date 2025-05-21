81b98347317072abe3f8cdb33166d317b6e82c4e
beppe <beppe.catanese@adyen.com>
2025-05-21 10:42:26 +0200

    Update phpdoc for BalanceControl models

--- src/Adyen/Model/BalanceControl/BalanceTransferRequest.php
+++ src/Adyen/Model/BalanceControl/BalanceTransferRequest.php
@@ -21,10 +21,7 @@ use Adyen\Model\BalanceControl\ObjectSerializer;
 /**
  * BalanceTransferRequest Class Doc Comment
  *
- * @category Class
  * @package  Adyen
- * @author   OpenAPI Generator team
- * @link     https://openapi-generator.tech
  * @implements \ArrayAccess<string, mixed>
  */
 class BalanceTransferRequest implements ModelInterface, ArrayAccess, \JsonSerializable
