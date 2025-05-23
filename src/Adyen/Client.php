4458a88008bdac434a5b0adeee17823a9db413ab
gcatanese <1771700+gcatanese@users.noreply.github.com>
2025-05-23 11:12:12 +0000

    chore(release): bump to 28.0.0

--- src/Adyen/Client.php
+++ src/Adyen/Client.php
@@ -7,7 +7,7 @@ use Adyen\HttpClient\CurlClient;
 
 class Client
 {
-    const LIB_VERSION = "27.0.0";
+    const LIB_VERSION = "28.0.0";
     const LIB_NAME = "adyen-php-api-library";
     const USER_AGENT_SUFFIX = "adyen-php-api-library/";
     const ENDPOINT_TEST = "https://pal-test.adyen.com";
