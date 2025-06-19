63cdd1c707b6896488592f843ab749527f590187
beppe <beppe.catanese@adyen.com>
2025-06-19 15:19:57 +0200

    Generate PaymentsApp

--- src/Adyen/Model/PaymentsApp/PaymentsAppResponse.php
+++ src/Adyen/Model/PaymentsApp/PaymentsAppResponse.php
@@ -21,10 +21,7 @@ use Adyen\Model\PaymentsApp\ObjectSerializer;
 /**
  * PaymentsAppResponse Class Doc Comment
  *
- * @category Class
  * @package  Adyen
- * @author   OpenAPI Generator team
- * @link     https://openapi-generator.tech
  * @implements \ArrayAccess<string, mixed>
  */
 class PaymentsAppResponse implements ModelInterface, ArrayAccess, \JsonSerializable
@@ -229,7 +226,7 @@ class PaymentsAppResponse implements ModelInterface, ArrayAccess, \JsonSerializa
      * @param mixed[] $data Associated array of property values
      *                      initializing the model
      */
-    public function __construct(array $data = null)
+    public function __construct(?array $data = null)
     {
         $this->setIfExists('paymentsApps', $data ?? [], null);
     }
