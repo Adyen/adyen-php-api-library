70cd90af27c2bdc7a78949893767c11a245d9534
beppe <beppe.catanese@adyen.com>
2025-06-19 12:00:08 +0200

    Generate and deprecate POSTerminalManagement

--- src/Adyen/Model/POSTerminalManagement/Address.php
+++ src/Adyen/Model/POSTerminalManagement/Address.php
@@ -259,7 +259,7 @@ class Address implements ModelInterface, ArrayAccess, \JsonSerializable
      * @param mixed[] $data Associated array of property values
      *                      initializing the model
      */
-    public function __construct(array $data = null)
+    public function __construct(?array $data = null)
     {
         $this->setIfExists('city', $data ?? [], null);
         $this->setIfExists('countryCode', $data ?? [], null);
--- src/Adyen/Model/POSTerminalManagement/AssignTerminalsRequest.php
+++ src/Adyen/Model/POSTerminalManagement/AssignTerminalsRequest.php
@@ -253,7 +253,7 @@ class AssignTerminalsRequest implements ModelInterface, ArrayAccess, \JsonSerial
      * @param mixed[] $data Associated array of property values
      *                      initializing the model
      */
-    public function __construct(array $data = null)
+    public function __construct(?array $data = null)
     {
         $this->setIfExists('companyAccount', $data ?? [], null);
         $this->setIfExists('merchantAccount', $data ?? [], null);
