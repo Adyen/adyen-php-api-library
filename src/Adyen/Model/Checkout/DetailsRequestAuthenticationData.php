871fac297abe7d36a56b2ec17c4a774b0965d706
beppe <beppe.catanese@adyen.com>
2025-07-24 12:32:52 +0200

    Generate Checkout models

--- src/Adyen/Model/Checkout/DetailsRequestAuthenticationData.php
+++ src/Adyen/Model/Checkout/DetailsRequestAuthenticationData.php
@@ -286,7 +286,7 @@ class DetailsRequestAuthenticationData implements ModelInterface, ArrayAccess, \
     /**
      * Sets authenticationOnly
      *
-     * @param bool|null $authenticationOnly If set to true, you will only perform the [3D Secure 2 authentication](https://docs.adyen.com/online-payments/3d-secure/other-3ds-flows/authentication-only), and not the payment authorisation. Default: *false**.
+     * @param bool|null $authenticationOnly Required to trigger the [authentication-only flow](https://docs.adyen.com/online-payments/3d-secure/authentication-only/). If set to **true**, you will only perform the 3D Secure 2 authentication, and will not proceed to the payment authorization.Default: **false**.
      *
      * @return self
      */
