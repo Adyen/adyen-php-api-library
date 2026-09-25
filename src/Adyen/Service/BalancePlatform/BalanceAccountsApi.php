0c9ad3ef593530e72cd51a918be8a2cd6a8107d8
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-09-25 11:07:06 +0000

    [balanceplatform] Automated update from Adyen/adyen-openapi@e96f4b3

--- src/Adyen/Service/BalancePlatform/BalanceAccountsApi.php
+++ src/Adyen/Service/BalancePlatform/BalanceAccountsApi.php
@@ -55,52 +55,6 @@ class BalanceAccountsApi extends Service
     }
 
     /**
-    * Create a sweep
-    *
-    * @param string $balanceAccountId
-    * @param \Adyen\Model\BalancePlatform\CreateSweepConfigurationV2 $createSweepConfigurationV2
-    * @param array|null $requestOptions
-    * @return \Adyen\Model\BalancePlatform\SweepConfigurationV2
-    * @throws AdyenException
-    */
-    public function createSweep(string $balanceAccountId, \Adyen\Model\BalancePlatform\CreateSweepConfigurationV2 $createSweepConfigurationV2, ?array $requestOptions = null): \Adyen\Model\BalancePlatform\SweepConfigurationV2
-    {
-        $endpoint = $this->baseURL . str_replace(['{balanceAccountId}'], [$balanceAccountId], "/balanceAccounts/{balanceAccountId}/sweeps");
-        $response = $this->requestHttp($endpoint, strtolower('POST'), (array) $createSweepConfigurationV2->jsonSerialize(), $requestOptions);
-        return ObjectSerializer::deserialize($response, \Adyen\Model\BalancePlatform\SweepConfigurationV2::class);
-    }
-
-    /**
-    * Delete a sweep
-    *
-    * @param string $balanceAccountId
-    * @param string $sweepId
-    * @param array|null $requestOptions
-
-    * @throws AdyenException
-    */
-    public function deleteSweep(string $balanceAccountId, string $sweepId, ?array $requestOptions = null)
-    {
-        $endpoint = $this->baseURL . str_replace(['{balanceAccountId}', '{sweepId}'], [$balanceAccountId, $sweepId], "/balanceAccounts/{balanceAccountId}/sweeps/{sweepId}");
-        $this->requestHttp($endpoint, strtolower('DELETE'), null, $requestOptions);
-    }
-
-    /**
-    * Get all sweeps for a balance account
-    *
-    * @param string $balanceAccountId
-    * @param array|null $requestOptions ['queryParams' => ['offset'=> int, 'limit'=> int]]
-    * @return \Adyen\Model\BalancePlatform\BalanceSweepConfigurationsResponse
-    * @throws AdyenException
-    */
-    public function getAllSweepsForBalanceAccount(string $balanceAccountId, ?array $requestOptions = null): \Adyen\Model\BalancePlatform\BalanceSweepConfigurationsResponse
-    {
-        $endpoint = $this->baseURL . str_replace(['{balanceAccountId}'], [$balanceAccountId], "/balanceAccounts/{balanceAccountId}/sweeps");
-        $response = $this->requestHttp($endpoint, strtolower('GET'), null, $requestOptions);
-        return ObjectSerializer::deserialize($response, \Adyen\Model\BalancePlatform\BalanceSweepConfigurationsResponse::class);
-    }
-
-    /**
     * Get all transaction rules for a balance account
     *
     * @param string $id
@@ -146,22 +100,6 @@ class BalanceAccountsApi extends Service
     }
 
     /**
-    * Get a sweep
-    *
-    * @param string $balanceAccountId
-    * @param string $sweepId
-    * @param array|null $requestOptions
-    * @return \Adyen\Model\BalancePlatform\SweepConfigurationV2
-    * @throws AdyenException
-    */
-    public function getSweep(string $balanceAccountId, string $sweepId, ?array $requestOptions = null): \Adyen\Model\BalancePlatform\SweepConfigurationV2
-    {
-        $endpoint = $this->baseURL . str_replace(['{balanceAccountId}', '{sweepId}'], [$balanceAccountId, $sweepId], "/balanceAccounts/{balanceAccountId}/sweeps/{sweepId}");
-        $response = $this->requestHttp($endpoint, strtolower('GET'), null, $requestOptions);
-        return ObjectSerializer::deserialize($response, \Adyen\Model\BalancePlatform\SweepConfigurationV2::class);
-    }
-
-    /**
     * Update a balance account
     *
     * @param string $id
@@ -175,22 +113,5 @@ class BalanceAccountsApi extends Service
         $endpoint = $this->baseURL . str_replace(['{id}'], [$id], "/balanceAccounts/{id}");
         $response = $this->requestHttp($endpoint, strtolower('PATCH'), (array) $balanceAccountUpdateRequest->jsonSerialize(), $requestOptions);
         return ObjectSerializer::deserialize($response, \Adyen\Model\BalancePlatform\BalanceAccount::class);
-    }
-
-    /**
-    * Update a sweep
-    *
-    * @param string $balanceAccountId
-    * @param string $sweepId
-    * @param \Adyen\Model\BalancePlatform\UpdateSweepConfigurationV2 $updateSweepConfigurationV2
-    * @param array|null $requestOptions
-    * @return \Adyen\Model\BalancePlatform\SweepConfigurationV2
-    * @throws AdyenException
-    */
-    public function updateSweep(string $balanceAccountId, string $sweepId, \Adyen\Model\BalancePlatform\UpdateSweepConfigurationV2 $updateSweepConfigurationV2, ?array $requestOptions = null): \Adyen\Model\BalancePlatform\SweepConfigurationV2
-    {
-        $endpoint = $this->baseURL . str_replace(['{balanceAccountId}', '{sweepId}'], [$balanceAccountId, $sweepId], "/balanceAccounts/{balanceAccountId}/sweeps/{sweepId}");
-        $response = $this->requestHttp($endpoint, strtolower('PATCH'), (array) $updateSweepConfigurationV2->jsonSerialize(), $requestOptions);
-        return ObjectSerializer::deserialize($response, \Adyen\Model\BalancePlatform\SweepConfigurationV2::class);
     }
 }
